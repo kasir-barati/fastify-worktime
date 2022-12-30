7521f75bb5acb03d72219b7087077da4d49c6e73
Kasir Barati <kasir.barati@gmail.com>
2022-12-30 13:43:53 +0100

    fix: ts type error for server.config.*

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,16 @@ import fastifyAutoload from '@fastify/autoload';
 // const server = fastify({ logger: process.env.something_that_must_be_in_env_file });
 // const server = fastify({ logger: fastify.config.something_that_must_be_in_env_file });
 const server = fastify({ logger: true });
+// For sake of Docker networking
 const HOST = '0.0.0.0';
 
-server.register(fastifyAutoload, {
-    dir: join(__dirname, 'plugins'),
-});
+async function main() {
+    await server.register(fastifyAutoload, {
+        dir: join(__dirname, 'plugins'),
+    });
 
-function main() {
     return server.listen({
-        port: Number('3000'),
-        // For sake of Docker networking
+        port: Number(server.config.PORT),
         host: HOST,
     });
 }
@@ -22,14 +22,14 @@ function main() {
 main()
     .then((result) => {
         server.log.info(
-            { port: '3000', host: HOST, result },
+            { port: server.config.PORT, host: HOST, result },
             ' server is up & runing',
         );
     })
     .catch((error) => {
         server.log.error(
             {
-                port: '3000',
+                port: server.config.PORT,
                 host: HOST,
                 error: { message: error.message },
             },
